454e0ead8bf8733c50805526ad7f87e3bdf8944e
shaunoconnell <shaunoconnell@gmail.com>
2011-06-22 16:01:51 -0400

    trivial implementation of general xml serialization

--- lib/simple_serialization.rb
+++ lib/simple_serialization.rb
@@ -1,25 +1,27 @@
 
+require 'rexml/document'
+require 'rexml/element'
+
 module SimpleSerialization
     module BasicXmlSerializatoin
       def to_xml
-
+        xml_doc = REXML::Document.new
+        xml_doc << REXML::XMLDecl.new
+        root_element = xml_doc.add_element self.class.to_s.downcase
+        serializable_attributes.each do |name, value|
+          attr_element = root_element.add_element name
+          attr_element.text = value
+        end
+        xml_doc.to_s
       end
 
       def serializable_attributes
         attrs = {}
-        self.instance_variables.each { |var| attrs[var.to_s[1..var.length]] = send(var.to_s[1..var.length])}
+        self.instance_variables.each { |var|
+          attrs[var.to_s[1..var.length]] = send(var.to_s[1..var.length])
+        }
         attrs
       end
+
     end
 end
-
-#class Widget
-#  include SimpleSerialization::BasicXmlSerializatoin
-#  attr_accessor :name
-#end
-#
-#
-#w = Widget.new
-#w.name = "alpha"
-#
-#puts w.serializable_attributes.class
--- spec/basic_xml_serialization_spec.rb
+++ spec/basic_xml_serialization_spec.rb
@@ -1,16 +1,37 @@
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
 require 'simple_serialization'
+require 'rexml/xpath'
+require 'rexml/document'
 
 class Widget
   include SimpleSerialization::BasicXmlSerializatoin
-  attr_accessor :name, :type
+  attr_accessor :name, :type, :description
 end
 
 describe Widget do
 
-  it "should figure out default serializable_attributes"
+  it "should figure out default serializable_attributes" do
+    widget = Widget.new
+    widget.name = "wingding"
+    widget.type = "font"
 
-  it "should correctly construct the xml"
+    widget.serializable_attributes.size.should == 2
+    widget.serializable_attributes['type'].should == "font"
+    widget.serializable_attributes['name'].should == "wingding"
+    widget.serializable_attributes['description'].should be_nil
+  end
+
+  it "should correctly construct the xml" do
+    widget = Widget.new
+    widget.name = "wingding"
+    widget.type = "font"
+
+    xml_doc = REXML::Document.new widget.to_xml
+    element = REXML::XPath.first(xml_doc, "/widget/name")
+    element.text.should == "wingding"
+    element = REXML::XPath.first(xml_doc, "/widget/type")
+    element.text.should == "font"
+  end
 end
 
